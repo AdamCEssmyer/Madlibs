989355f884986cc6f880a3aa0e6ca657708d83a6
Adam Essmyer <acemyer@Adams-MBP.lan1>
2021-03-22 19:58:06 -0500

    made variables for writing the story

--- Madlibs.py
+++ Madlibs.py
@@ -1,4 +1,13 @@
+    #Loop back to this point once code finishes
 loop = 1
 while (loop < 10):
 
     noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
+    #Displays the story based on the users input
+    print("--------------------------------------")
+    print ("Beyond the pale light", noun," the ", p_noun)
